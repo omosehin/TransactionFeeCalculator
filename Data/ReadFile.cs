48b2a22ee0cfff7437e3e82e05e0741ee97232e3
Abayomi Omosehin <abayomi.omosehin@venturegardengroup.com>
2020-10-16 10:06:51 -1100

    separate method

--- Data/ReadFile.cs
+++ Data/ReadFile.cs
@@ -14,14 +14,24 @@ namespace TransactionFeeCalculator
         }
         public TransactionDetails Calculatefee()
         {
+            //var fileJson = File.ReadAllText(@"C:\Users\ayobami.omosehin\Documents\TransactionFeeCalculator\Data\chargeFee.Json");
+            string relativePath = "..\\Data\\chargeFee.Json";
+            string fullPathToFirstFile = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(relativePath), "chargeFee.Json"));
+            string filePath = fullPathToFirstFile.Replace("\\bin\\Debug", "");
+            var fileJson = File.ReadAllText(filePath);
+
+            var transactionData = JsonConvert.DeserializeObject<List<TransactionFee>>(fileJson);
+            
+            return getTransferDetails(transactionData);
+        }
+
+        private TransactionDetails getTransferDetails(List<TransactionFee> transactionData)
+        {
             int charge = 0;
             long TotalAmount = 0;
-
-            var transaction = File.ReadAllText(@"C:\Users\ayobami.omosehin\Documents\TransactionFeeCalculator\Data\chargeFee.Json");
-            dynamic transactionData = JsonConvert.DeserializeObject<List<TransactionFee>>(transaction);
             foreach (var item in transactionData)
             {
-                if(_amount>= item.MinAmount && _amount <= item.MaxAmount)
+                if (_amount >= item.MinAmount && _amount <= item.MaxAmount)
                 {
                     TotalAmount = _amount + item.FeeAmount;
                     charge = item.FeeAmount;
@@ -30,6 +40,5 @@ namespace TransactionFeeCalculator
 
             return new TransactionDetails { AmountSending = _amount, TotalAmount = TotalAmount, TransferCharge = charge };
         }
-
     }
 }
